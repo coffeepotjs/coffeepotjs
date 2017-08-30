664ec4fcba35e7f343ae6ebdc8306485f130d96d
xerocole <development@crazycamel.ca>
2017-08-30 16:29:07 -0400

    [espresso] initial commit

--- src/drink/espresso/serve.js
+++ src/drink/espresso/serve.js
@@ -7,13 +7,42 @@ define( [ "jquery", "bean/utils" ], function( $, Util ) {
 	"use strict";
 
 	var selector = "[data-coffeepot^=americano]",
+        defaults = { type: "GET", cache: false, datatype: "xml" },
+        canvas = ".cp-canvas",
 		order = $( selector );
 
+
+	var formats = [
+	  function ( xml ) { }
+    ];
+
+        /*type: "GET",
+    url: url,
+        cache: false,
+        dataType: "xml",
+        success: function( xml ) {
+        $( xml ).find( "entry" ).each( function() {
+            var entry = $( this );
+            console.log( "=====================" );
+            console.log( entry.find( "title" ).text() );
+            console.log( "=====================" );
+        } );
+    }
+}*/
+function get( options )
+    {
+        var settings = $.extend( {}, defaults, options );
+        $.ajax( settings );
+    }
+
+	// Main
 	for ( var item = 0; item < order.length; item++ )
 	{
 
 		var $elm = order.eq( item ),
 			action = Util.parseCommand(  $elm );
+
+
 
 
 		$elm.addClass( "cp-init" );
